3d7dc3c1b594e858fa49d783e5afaab54c21c553
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-21 14:51:33 +0200

    Create and style header

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab'
 
 const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+  return <div>Map</div>;
+};
 
-export default Map
+export default Map;
